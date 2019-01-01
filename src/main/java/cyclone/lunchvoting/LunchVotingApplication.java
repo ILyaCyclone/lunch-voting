4a780e3b0069602c2a9c32d558280a114fc9e6c7
ILya Cyclone <ilya.cyclone@gmail.com>
2019-01-01 23:11:37 +0300

    initialize Spring Boot project

---
+++ src/main/java/cyclone/lunchvoting/LunchVotingApplication.java
@@ -0,0 +1,14 @@
+package cyclone.lunchvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchVotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchVotingApplication.class, args);
+    }
+
+}
+
